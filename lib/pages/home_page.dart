fc89986e02d3c7807b4bbd1090f84f98aa743ca9
Joaremio <joaremiorevoredo@gmail.com>
2026-06-18 21:37:01 -0300

    feat: integra pagina de transação a home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,11 +15,7 @@ class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
   static const _titles = ['Dashboard', 'Transações', 'Perfil'];
-  static const _pages = [
-    DashboardPage(),
-    TransactionsPage(),
-    ProfilePage(),
-  ];
+  static const _pages = [DashboardPage(), TransactionsPage(), ProfilePage()];
 
   @override
   Widget build(BuildContext context) {
